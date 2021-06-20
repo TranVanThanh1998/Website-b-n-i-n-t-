7a5c2c3f4cebf8c7eb0683b7f03f6e54affa50ee
TranVanThanh1998 <84833621+TranVanThanh1998@users.noreply.github.com>
2021-06-21 02:40:49 +0700

    Add files via upload

---
+++ ~AutoRecover.UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UngDung.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            var user = new userDAO();
+            var userList = user.ListAll();
+            return View(userList);
+        }
+    }
+}
